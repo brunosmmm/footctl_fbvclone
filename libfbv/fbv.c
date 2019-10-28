5c264c89fd8970cde8558e9e472e0544994d8c88
Bruno Morais <brunosmmm@gmail.com>
2019-10-28 16:22:54 -0400

    implement initial FBV library

---
+++ libfbv/fbv.c
@@ -0,0 +1,147 @@
+#include "fbv.h"
+#include <string.h>
+
+// possible states
+#define FBV_STATE_RX_HDR 0
+#define FBV_STATE_RX_LEN 1
+#define FBV_STATE_RX_CMD 2
+#define FBV_STATE_RX_PRM 3
+
+// internal flags
+#define FBV_FLAG_INIT 0x01
+
+// user flag mask
+#define FBV_USR_FLAG_MASK 0xF0
+
+// other
+#define RX_BUFFER_SIZE MAX_PARAM_SIZE + 1
+
+typedef struct fbv_state_machine_s {
+  uint8_t state;
+  uint8_t flags;
+  uint8_t rxBuffer[RX_BUFFER_SIZE];
+  uint8_t wrPtr;
+  uint8_t pendingBytes;
+  FBVStateMachineConfig* cfg;
+} FBVStateMachine;
+
+
+static FBVStateMachine fsm;
+
+// done receiving packet
+static void fbv_rx_done(void) {
+  FBVMessage msg;
+  fsm.wrPtr = 0;
+
+  // save message
+  msg.paramSize = fsm.rxBuffer[0] - 1;
+  msg.msgType = fsm.rxBuffer[1];
+  memcpy(&msg.params, fsm.rxBuffer+2, fsm.rxBuffer[0] - 2);
+
+  if (fsm.cfg) {
+    // callback with received message
+    if (fsm.cfg->msgRx) {
+      (fsm.cfg->msgRx)(msg);
+    }
+  }
+}
+
+// get flags
+uint8_t FBV_get_flags(void) {
+  uint8_t flags = fsm.flags;
+  fsm.flags &= ~FBV_USR_FLAG_MASK;
+  return flags & FBV_USR_FLAG_MASK;
+}
+
+// Initialize state machine
+void FBV_initialize(FBVStateMachineConfig* cfg) {
+  fsm.state = FBV_STATE_RX_HDR;
+  fsm.cfg = cfg;
+  fsm.wrPtr = 0;
+  fsm.pendingBytes = 0;
+  fsm.flags = FBV_FLAG_INIT;
+}
+
+// receive byte and parse
+void FBV_recv_byte(uint8_t byte) {
+  if (!(fsm.flags & FBV_FLAG_INIT)){
+    // not initialized
+    return;
+  }
+
+  switch (fsm.state) {
+  case FBV_STATE_RX_HDR:
+    if (byte != 0xF0) {
+      break;
+    }
+    fsm.state = FBV_STATE_RX_LEN;
+    break;
+  case FBV_STATE_RX_LEN:
+    if (byte > MAX_PARAM_SIZE) {
+      byte = MAX_PARAM_SIZE;
+    }
+    fsm.pendingBytes = byte;
+    fsm.state = FBV_STATE_RX_CMD;
+    break;
+  case FBV_STATE_RX_CMD:
+    fsm.rxBuffer[fsm.wrPtr++] = byte;
+    fsm.pendingBytes--;
+    fsm.state = FBV_STATE_RX_PRM;
+    break;
+  case FBV_STATE_RX_PRM:
+    fsm.rxBuffer[fsm.wrPtr++] = byte;
+    fsm.pendingBytes--;
+    if (!fsm.pendingBytes) {
+      fsm.state = FBV_STATE_RX_HDR;
+      fbv_rx_done();
+      break;
+    }
+    break;
+  default:
+    fsm.pendingBytes = 0;
+    fsm.wrPtr = 0;
+    fsm.state = FBV_STATE_RX_HDR;
+    fsm.flags |= FBV_FLAG_ERR;
+    break;
+  }
+
+}
+
+// receive multiple bytes
+void FBV_recv_bytes(uint8_t* bytes, unsigned int size) {
+  while (size) {
+    FBV_recv_byte(*bytes);
+    bytes++;
+    size--;
+  }
+}
+
+// send message
+void FBV_send_msg(FBVMessage* msg) {
+  unsigned int paramSize = 0;
+  if (!msg) {
+    return;
+  }
+
+  if (!(fsm.flags & FBV_FLAG_INIT)) {
+    return;
+  }
+
+  // just call the send byte function if available
+  if (fsm.cfg) {
+    if (fsm.cfg->msgTx) {
+      // send header
+      (fsm.cfg->msgTx)(0xF0);
+      // send length
+      (fsm.cfg->msgTx)(msg->paramSize + 2);
+      // send command
+      (fsm.cfg->msgTx)(msg->msgType);
+      // send params
+      paramSize = msg->paramSize;
+      while (paramSize) {
+        (fsm.cfg->msgTx)(msg->params[msg->paramSize-paramSize]);
+        paramSize--;
+      }
+    }
+  }
+}
--- libfbv/fbv.h
+++ libfbv/fbv.h
@@ -1,4 +1,62 @@
 #ifndef _FBV_H_INCLUDED_
 #define _FBV_H_INCLUDED_
 
+#include <stdint.h>
+
+// maximum parameter payload size
+#define MAX_PARAM_SIZE 18
+
+// user readable flags
+#define FBV_FLAG_ERR 0x10
+
+typedef enum fbv_message_type_e
+  {
+   FBV_SET_LED = 0x04,
+   FBV_SET_CH = 0x0C,
+   FBV_SET_TXT = 0x10,
+   FBV_ACK_PING = 0x01,
+   FBV_SET_BNK1 = 0x0A,
+   FBV_SET_BNK2 = 0x0B,
+   FBV_TUN_STAT = 0x08,
+   FBV_BTN_STAT = 0x81,
+   FBV_CTL_STAT = 0x82,
+   FBV_HNDSHAKE = 0x30
+  } FBVMessageType;
+
+typedef enum fbv_led_type_e
+  {
+   FBV_LED_TAP = 0x61,
+   FBV_LED_MOD = 0x41,
+   FBV_LED_DLY = 0x51,
+   FBV_LED_CHA = 0x20,
+   FBV_LED_CHB = 0x30,
+   FBV_LED_CHC = 0x40,
+   FBV_LED_CHD = 0x50,
+   FBV_LED_SB1 = 0x12,
+   FBV_LED_SB2 = 0x22,
+   FBV_LED_SB3 = 0x32,
+   FBV_LED_AMP = 0x01,
+   FBV_LED_REV = 0x21
+  } FBVLED;
+
+typedef struct fbv_message_s {
+  FBVMessageType msgType;
+  uint8_t params[MAX_PARAM_SIZE];
+  uint8_t paramSize;
+} FBVMessage;
+
+typedef void (*FBVMessageCallback)(FBVMessage);
+typedef void (*FBVMessageSendByte)(uint8_t);
+
+typedef struct fbv_fsm_cfg_s {
+  FBVMessageCallback msgRx;
+  FBVMessageSendByte msgTx;
+} FBVStateMachineConfig;
+
+uint8_t FBV_get_flags(void);
+void FBV_initialize(FBVStateMachineConfig* cfg);
+void FBV_recv_byte(uint8_t byte);
+void FBV_recv_bytes(uint8_t* bytes, unsigned int size);
+void FBV_send_msg(FBVMessage* msg);
+
 #endif

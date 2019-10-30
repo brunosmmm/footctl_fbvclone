b1228a17535b3428f2af348efb6f9e41858ba584
Bruno Morais <brunosmmm@gmail.com>
2019-10-29 22:15:41 -0400

    implement stubs for time primitive

---
+++ footctl/time.c
@@ -0,0 +1,6 @@
+#include "time.h"
+
+tick_t TIME_get(void) {
+  // get current time
+  return 0;
+}
---
+++ footctl/time.h
@@ -0,0 +1,9 @@
+#ifndef _TIME_H_INCLUDED_
+#define _TIME_H_INCLUDED_
+
+#include <stdint.h>
+#include <config.h>
+
+tick_t TIME_get(void);
+
+#endif

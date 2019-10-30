6dfcb992d747c33f2faf8be3a0ace934a5d31bf7
Bruno Morais <brunosmmm@gmail.com>
2019-10-29 22:15:50 -0400

    implement simple debouncing algorithm

--- footctl/io.c
+++ footctl/io.c
@@ -1,6 +1,10 @@
 #include "io.h"
 #include "config.h"
+#include "time.h"
 #include <string.h>
+
+#define BTN_POLL_INTERVAL 20
+#define BTN_DEBOUNCE_COUNT 2
 
 typedef struct btn_control_s {
   BTNEventCallback evtCb;
@@ -21,8 +25,42 @@ uint32_t BTNS_get_state(void) {
   return btns.buttonStates;
 }
 
-void BTNS_cycle(void) {
+static uint32_t _read_btns(void) {
+  // do something
+  return 0;
+}
 
+void BTNS_cycle(void) {
+  unsigned int i = 0;
+  uint32_t btn_state = 0;
+  if (TIME_get() - btns.lastCycle < BTN_POLL_INTERVAL) {
+    return;
+  }
+
+  btn_state = _read_btns();
+  for (i=0; i<IO_LED_COUNT;i++) {
+    if ((btn_state & (1<<i)) ^ (btns.buttonStates & (1<<i))) {
+      // states differ for this particular bit
+      if (btns.transientStates[i] < BTN_DEBOUNCE_COUNT) {
+        // increment debounce count
+        btns.transientStates[i]++;
+      }
+      else {
+        // commit state
+        if (btn_state & (1<<i)) {
+          btns.buttonStates |= (1<<i);
+        } else {
+          btns.buttonStates &= (1<<i);
+        }
+        // reset debounce counter
+        btns.transientStates[i] = 0;
+        // call event callback
+        if (btns.evtCb) {
+          (btns.evtCb)(i, (btn_state & (1<<i) ? 1: 0));
+        }
+      }
+    }
+  }
 }
 
 void LEDS_set_state(uint32_t led_states) {
--- footctl/io.h
+++ footctl/io.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 
+#define BTN_CHA 0x0
+#define BTN_CHB 0x1
+#define BTN_CHC 0x2
+#define BTN_CHD 0x3
+#define BTN_EQ 0x4
+#define BTN_STOMP 0x5
+#define BTN_MOD 0x6
+#define BTN_DLY 0x7
+#define BTN_UP 0x8
+#define BTN_DN 0x9
+#define BTN_WAH 0xa
+#define BTN_TAP 0xb
+
 typedef void (*BTNEventCallback)(uint8_t, uint8_t);
 
 void BTNS_initialize(BTNEventCallback callback);

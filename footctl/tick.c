307e2935ae35728b34de9f083b68a0a17fb88cfb
Bruno Morais <brunosmmm@gmail.com>
2019-10-30 12:48:03 -0400

    implement virtual tick for debugging

--- footctl/tick.c
+++ footctl/tick.c
@@ -1,9 +1,20 @@
 #include "tick.h"
 #ifdef VIRTUAL_HW
-#define _POSIX_C_SOURCE 200809L
 #include <time.h>
 #include <stdio.h>
 static struct timespec _VHW_initial_time;
+static inline struct timespec timeDiff(struct timespec oldTime, struct timespec time) {
+  if (time.tv_nsec < oldTime.tv_nsec)
+    return (struct timespec){
+      tv_sec : time.tv_sec - 1 - oldTime.tv_sec,
+      tv_nsec : 1E9 + time.tv_nsec - oldTime.tv_nsec
+    };
+  else
+    return (struct timespec){
+      tv_sec : time.tv_sec - oldTime.tv_sec,
+      tv_nsec : time.tv_nsec - oldTime.tv_nsec
+    };
+}
 #endif
 
 void TICK_initialize(void) {
@@ -16,7 +27,12 @@ void TICK_initialize(void) {
 tick_t TICK_get(void) {
   // get current time
 #ifdef VIRTUAL_HW
-
+  struct timespec now, diff;
+  unsigned long ms = 0;
+  clock_gettime(CLOCK_MONOTONIC, &now);
+  diff = timeDiff(_VHW_initial_time, now);
+  ms = diff.tv_sec*1000 + diff.tv_nsec / 1000000;
+  return (time_t)ms;
 #endif
   return 0;
 }

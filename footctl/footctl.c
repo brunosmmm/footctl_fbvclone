dab2e616cef24e9f6e17e6eedfece4c34c330c11
Bruno Morais <brunosmmm@gmail.com>
2019-11-01 17:03:36 -0400

    update implementation with GPIO pins

--- footctl/footctl.c
+++ footctl/footctl.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "virtual.h"
+#else
+#include "stm32.h"
 #endif
 
 int main(void) {
@@ -12,6 +14,8 @@ int main(void) {
 #ifdef VIRTUAL_HW
   printf("INFO: initializing\n");
   VIRTUAL_initialize();
+#else
+  SYSTEM_initialize();
 #endif
 
   // initialize
